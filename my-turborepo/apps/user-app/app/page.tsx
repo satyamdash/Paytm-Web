40cce4aaed1636f7ce625509a91a2d673aa96020
Satyam Gupta <satyam.bn6@gmail.com>
2025-03-23 01:40:50 +0530

    Added Prisma

--- my-turborepo/apps/user-app/app/page.tsx
+++ my-turborepo/apps/user-app/app/page.tsx
@@ -1,5 +1,13 @@
-import { Button } from "@repo/ui/button";
-export default function Home() {
+import { PrismaClient } from "@repo/db";
+
+import dotenv from "dotenv";
+dotenv.config({ path: "../../packages/db/.env" });
+
+const prisma = new PrismaClient();
+
+export default async function Home() {
+  const users = await prisma.user.findMany();
+  console.log(users);
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-100">
       <h1 className="text-4xl font-bold text-blue-600">
